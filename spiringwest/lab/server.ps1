57dd9d502684aff1594fe2114de928ccdbb87def
fbarker92 <72350921+fbarker92@users.noreply.github.com>
2025-01-17 17:55:11 +0000

    Update server.ps1

--- spiringwest/lab/server.ps1
+++ spiringwest/lab/server.ps1
@@ -2,12 +2,13 @@ Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass -Force
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 $downloadUri = 'https://raw.githubusercontent.com/fbarker92/rmm/refs/heads/main/spiringwest/lab/trmm-springwest-lab-server-amd64.exe'
+$apiUri = $downloadUri.split('/')
 $outPath = $env:TMP
 $outFile = $downloadUri.Split('/')[-1]
 
 Try
     {
-        $DefenderStatus = Get-MpComputerStatus | select  AntivirusEnabled
+        $DefenderStatus = Get-MpComputerStatus | Select-Object  AntivirusEnabled
         if ($DefenderStatus -match "True") {
             Add-MpPreference -ExclusionPath 'C:\Program Files\TacticalAgent\*'
             Add-MpPreference -ExclusionPath 'C:\Program Files\Mesh Agent\*'
@@ -23,12 +24,12 @@ Try
       Write-Output "Waiting for network"
       Start-Sleep -s 5
       $X += 1      
-    } until(($connectresult = Test-NetConnection $apilink[2] -Port 443 | ? { $_.TcpTestSucceeded }) -or $X -eq 3)
+    } until(($connectresult = Test-NetConnection $apiUri[2] -Port 443 | Where-Object { $_.TcpTestSucceeded }) -or $X -eq 3)
     
     if ($connectresult.TcpTestSucceeded -eq $true){
         Try
         {  
-            Invoke-WebRequest -Uri $downloadlink -OutFile $outPath\$outFile
+            Invoke-WebRequest -Uri $downloadUri -OutFile "$outPath\$outFile"
             Start-Process -FilePath $OutPath\$output -ArgumentList ('/VERYSILENT /SUPPRESSMSGBOXES') -Wait
             exit 0
         }
@@ -41,7 +42,7 @@ Try
         }
         Finally
         {
-            Remove-Item -Path $outPath\$outFile
+            Remove-Item -Path "$outPath\$outFile"
         }
     } else {
         Write-Output "Unable to connect to server"
